51d3bde47904a079a09369aafd3d67b97fc146ba
Mrugen Deshmukh <mrugenmike@gmail.com>
2015-04-06 13:11:29 -0700

    Fixed the unit test for JobResource

--- src/test/java/com/pronet/search/JobSearchResourceTest.java
+++ src/test/java/com/pronet/search/JobSearchResourceTest.java
@@ -4,20 +4,28 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 @RunWith(value = MockitoJUnitRunner.class)
 public class JobSearchResourceTest {
 @Test
-public void itShouldSendJobsBySearchTerm(){/*
+public void itShouldSendJobsBySearchTerm(){
     //given
     String term = "Software Engineer";
     JobSearchService jobSearchService = mock(JobSearchService.class);
-    when(jobSearchService.fetchJobListings(term)).thenReturn(Arrays.asList(new JobListing("jobid","title","ebay","logo","SF","Great Job!")));
+    final List<JobListing> jobListings = Arrays.asList(new JobListing("jobid", "title", "ebay", "logo", "SF", "Great Job!"));
+    when(jobSearchService.fetchJobListings(term,1,1)).thenReturn(new JobListings(jobListings,1l));
     JobSearchResource jobSearchResource = new JobSearchResource(jobSearchService);
     //when
 
-    List<JobListing> jobList = jobSearchResource.fetchJobs(term);
+    JobListings jobList = jobSearchResource.fetchJobListings(term,1,1);
     //then
     assertThat(jobList).isNotNull();
-    assertThat(jobList).hasSize(1);*/
+    assertThat(jobList.getListings()).hasSize(1);
 }
 }
