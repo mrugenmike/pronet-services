6afe77624beb8d8c8ea2438287a3fbb19481fa55
Mrugen Deshmukh <mrugenmike@gmail.com>
2015-04-11 21:42:19 -0700

    fixed autowiring issue with companyService

--- src/main/java/com/pronet/search/company/CompanySearchResource.java
+++ src/main/java/com/pronet/search/company/CompanySearchResource.java
@@ -1,6 +1,7 @@
 package com.pronet.search.company;
 
 import com.pronet.exceptions.NoContentException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class CompanySearchResource {
 
-
+    @Autowired
     private CompanySearchService companySearchService;
 
     @RequestMapping("companies")
