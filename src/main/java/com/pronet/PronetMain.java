c1c897ced2268728a03b3f78027bed2fc1adc317
Mrugen Deshmukh <mrugenmike@gmail.com>
2015-04-12 15:50:35 -0700

    Converted artifact to war for beanstalk deployment

--- src/main/java/com/pronet/PronetMain.java
+++ src/main/java/com/pronet/PronetMain.java
@@ -2,9 +2,16 @@ package com.pronet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PronetMain {
+public class PronetMain extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PronetConfig.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PronetConfig.class, args);
