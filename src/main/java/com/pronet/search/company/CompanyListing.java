6f412bb953661585fa0f6b4992ff13181390bf4d
Mrugen Deshmukh <mrugenmike@gmail.com>
2015-04-11 21:39:44 -0700

    added description for company listings

--- src/main/java/com/pronet/search/company/CompanyListing.java
+++ src/main/java/com/pronet/search/company/CompanyListing.java
@@ -2,9 +2,7 @@ package com.pronet.search.company;
 
 import java.util.Map;
 
-import static com.pronet.search.company.CompanyFields.COMAPANYLOGO;
-import static com.pronet.search.company.CompanyFields.COMPANYID;
-import static com.pronet.search.company.CompanyFields.COMPANYNAME;
+import static com.pronet.search.company.CompanyFields.*;
 
 public class CompanyListing {
     String id;
@@ -12,17 +10,19 @@ public class CompanyListing {
     String companyName;
     String description;
 
-    public CompanyListing(String id, String companyLogo, String companyName) {
+    public CompanyListing(String id, String companyLogo, String companyName, String description) {
         this.id = id;
         this.logo = companyLogo;
         this.companyName = companyName;
+        this.description = description;
     }
 
     public static CompanyListing instance(Map<Object,Object> company){
         final String id = company.get(COMPANYID).toString();
         final String companyLogo = company.get(COMAPANYLOGO).toString();
         final String companyName = company.get(COMPANYNAME).toString();
-        return new CompanyListing(id,companyLogo,companyName);
+        final String description = company.get(COMPANYDESC).toString();
+        return new CompanyListing(id,companyLogo,companyName,description);
     }
 
 }
